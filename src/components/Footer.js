a78b29b8609a165f103c363bd5753819f7220758
dainylcua <dainylcua@gmail.com>
2021-12-06 14:00:58 -0600

    Adds more content and installs fontawesome

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,40 @@
-import { useState, useEffect } from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faTwitterSquare } from "@fortawesome/free-brands-svg-icons"
+import { faGithubSquare } from "@fortawesome/free-brands-svg-icons"
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons"
 
-const Footer = (props) => {
-  
-
-  return(
-    <div className="bg-purple-600">
-      <div className="flex justify-between w-4/5 py-8 mx-auto md:w-3/5">
-        footer
+const Footer = () => {
+    return(
+      <div className="py-20 bg-gray-900">
+        <footer className="flex items-center justify-between w-4/5 h-16 py-8 mx-auto text-xl text-white md:w-3/5 md:text-2xl">
+            <div>
+                Made by Dainyl Cua
+            </div>
+            <div>
+                <div className="flex flex-row items-center space-x-4">
+                    <div className="hidden md:visible">
+                        Find me here:
+                    </div>
+                    <div>
+                        <a href="https://twitter.com/dainylcua">
+                            <FontAwesomeIcon icon={faTwitterSquare} size="2x" />
+                        </a>
+                    </div>
+                    <div>
+                        <a href="https://www.linkedin.com/in/dainylcua/">
+                            <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                        </a>
+                    </div>
+                    <div>
+                    <a href="https://github.com/dainylcua">
+                            <FontAwesomeIcon icon={faGithubSquare} size="2x" />
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </footer>
       </div>
-    </div>
-  )
-}
+    )
+} 
 
 export default Footer
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -21,7 +21,9 @@ const Projects = (props) => {
               <div>Badges</div>
             </div>
             <div className="flex flex-col">
-              <div>DungeonSheet</div>
+              <div>
+                <a href="https://dc-dungeon-sheet.netlify.app/">DungeonSheet</a>
+              </div>
               <div>DungeonSheet was my first big project: a single-page-application created with JavaScript and jQuery that has no frameworks. This app uses the Dungeons and Dragons 5th edition API to create a level 1 character. The API used can be found <a href="http://www.dnd5eapi.co/">here!</a></div>
             </div>
           </div>
@@ -31,8 +33,12 @@ const Projects = (props) => {
               <div>Badges</div>
             </div>
             <div className="flex flex-col">
-              <div>5eTale</div>
-              <div>5eTale is a full-stack CRUD app that uses Embedded JavaScript. </div>
+              <div>
+                <a href="https://dc-5etale.herokuapp.com/">
+                  5eTale
+                </a>
+              </div>
+              <div>5eTale is a full-stack CRUD app that uses MonogoDB, Embedded JavaScript, Express.js, Node.js. Create custom items for D&D 5e and share them with other users!</div>
             </div>
           </div>
           <div className="flex flex-row">
